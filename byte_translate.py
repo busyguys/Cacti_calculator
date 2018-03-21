83d716e2280afb91493d5c461c2e4d5e06cb8387
이태우(Taewoo Lee) <busyguys@korea.ncsoft.corp>
2018-03-21 13:17:52 +0900

    ADD_TRANSACTION

---
+++ byte_translate.py
@@ -0,0 +1,25 @@
+import re
+
+
+sample1=11443771980.2
+sample2=318042091.235
+sample3=10063991671.3
+
+samples=[sample1,sample2,sample3]
+
+giga=1000000000
+mega=1049000
+kilo=1000
+
+if __name__=="__main__":
+    print "Hello!"
+    #print samples
+    for a in samples:
+        if(a>giga):
+            print a/giga, "GB"
+        elif(a>mega):
+            print a/mega, "MB"
+        elif(a>kilo):
+            print a/kilo, "KB"
+        else:
+            print a, "B"
---
+++ version_3/byte_translate.py
@@ -0,0 +1,8 @@
+import re
+
+
+sample1=11443771980.2
+sample2=
+
+if __name__=="__main__":
+    print "Hello!"
